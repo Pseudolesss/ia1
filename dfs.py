2e707121ba6e3d74c9041c0de2add4472543e743
Pseudoless <julienlhoest@hotmail.com>
2018-10-08 22:03:50 +0200

    update dfs.py (prototype)

--- dfs.py
+++ dfs.py
@@ -1,7 +1,7 @@
 # Complete this class for all parts of the project
 
 from pacman_module.game import Agent
-from pacman_module.pacman import Directions
+from pacman_module.pacman import Directions as dir
 
 
 class PacmanAgent(Agent):
@@ -11,6 +11,8 @@ class PacmanAgent(Agent):
         ----------
         - `args`: Namespace of arguments from command-line prompt.
         """
+        self.visited = dict()
+        self.path = dict()
         self.args = args
 
     def get_action(self, state):
@@ -24,4 +26,30 @@ class PacmanAgent(Agent):
         -------
         - A legal move as defined in `game.Directions`.
         """
-        pass
+        self.path.clear()
+        
+        visited = self.visited.get(state, False)
+        
+        if visited:
+            return visited #add the brand new state and corresponding action to visited
+        else:
+            path = self.get_path(state)
+            self.visited.update(path)
+            return path.get(state) #value associated to key state 
+    
+    def get_path(self, state):
+        """
+        Given a pacman game state, returns a dictionary.
+        Arguments:
+        ----------
+        - `state`: the current game state.
+        Return:
+        -------
+        - A dictionary with states as keys and directions as values.
+        Each state is part of the DFS path solution.
+        """
+        if state.isWin():
+            return {state : dir.STOP}  
+        else:
+            successors = state.generatePacmanSuccessors()
+            return self.path.update(self.get_path(successors[0][0]))
