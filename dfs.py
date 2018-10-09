f99cc7f7fe32c18fb28b1ce306debb891bc82177
Pseudoless <julienlhoest@hotmail.com>
2018-10-09 21:36:39 +0200

    (working version)

--- dfs.py
+++ dfs.py
@@ -12,7 +12,6 @@ class PacmanAgent(Agent):
         - `args`: Namespace of arguments from command-line prompt.
         """
         self.visited = dict()
-        self.path = dict()
         self.args = args
 
     def get_action(self, state):
@@ -27,17 +26,17 @@ class PacmanAgent(Agent):
         - A legal move as defined in `game.Directions`.
         """
         key = ( state.getPacmanPosition(), state.getFood() ) #key for dict
-        self.path.clear() #clear obsolete results
         
         visited = self.visited.get(key, False)
         
         if visited:
-            return visited[1] #return already compute result
+            return visited #return already compute result
         else:
-            path = self.get_path(state)
-            return path.get(key)[1] #value associated to key (position, food) 
+            path = self.get_path(state, [])
+            self.visited.update(path)
+            return path.get(key) #value associated to key (position, food) 
     
-    def get_path(self, state):
+    def get_path(self, state, visited):
         """
         Given a pacman game state, returns a dictionary.
         Arguments:
@@ -48,20 +47,35 @@ class PacmanAgent(Agent):
         - A dictionary with pair (Pacman pos, food pos) as keys and coresponding states
         and directions pairs as values.
         Each state is part of the DFS path solution starting from state.
-        """        
-        print(state)
-        print( state.isWin())
+        """
+        visited.append((state.getPacmanPosition(), state.getFood()))
         if state.isWin():
-            return { ( state.getPacmanPosition(), state.getFood() ) : (state, dir.STOP)}  
+            return { ( state.getPacmanPosition(), state.getFood() ) : dir.STOP}  
         else:
+            path = dict()
+            next_call = dict()
             successors = state.generatePacmanSuccessors()
             
+            key = (state.getPacmanPosition(), state.getFood())
+            i=0
             for son in successors:
-                key = (son[0].getPacmanPosition(), son[0].getFood())
-                if not self.visited.get(key, False):
-                    self.visited.update({key : (state, son[1])}) #add the brand new states and corresponding actions to visited
+                """for v in visited:
+                    print(v[0]),
+                print()
+                print(son[0].getPacmanPosition())"""
+                if not (son[0].getPacmanPosition(), son[0].getFood()) in visited: #self.visited.get(key1, False):
+                 #   self.visited.update({key : son[1]}) #add the brand new state and corresponding action to self.visited
+                    next_call = self.get_path(son[0], visited)
+                    if len(next_call) == 0:
+                        i+=1
+                        continue
                     break
+                i+=1
+                
+            #print(len(next_call), i)
+            if i == len(successors): # No sons converge to solution
+                return dict()
             
-            self.path.update({key : (state, son[1])})
-            self.path.update(self.get_path(son[0]))
-            return self.path
+            path.update({key : son[1]}) # Add of the {state : action} of the current call 
+            path.update(next_call) # Add result of the selected son
+            return path
