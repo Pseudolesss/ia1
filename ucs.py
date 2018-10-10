7fdc7b6c6de29f4da37be4c6b155f5d89a9669cf
Pseudoless <julienlhoest@hotmail.com>
2018-10-10 21:31:46 +0200

    Problem Priority queue (unique priority value alowed)

--- ucs.py
+++ ucs.py
@@ -1,8 +1,25 @@
 # Complete this class for all parts of the project
 
 from pacman_module.game import Agent
-from pacman_module.pacman import Directions
+from pacman_module.pacman import Directions as dir
+import queue
+import inspect
 
+# To avoid conflict with entries with the same priority integer
+class PriorityEntry(object):
+
+    def __init__(self, priority, data):
+        self.data = data
+        self.priority = priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+    
+    def priority(self):
+        return self.priority
+    
+    def data(self):
+        return self.data
 
 class PacmanAgent(Agent):
     def __init__(self, args):
@@ -11,6 +28,7 @@ class PacmanAgent(Agent):
         ----------
         - `args`: Namespace of arguments from command-line prompt.
         """
+        self.computed = dict()
         self.args = args
 
     def get_action(self, state):
@@ -24,4 +42,76 @@ class PacmanAgent(Agent):
         -------
         - A legal move as defined in `game.Directions`.
         """
-        pass
+        key = ( state.getPacmanPosition(), state.getFood() ) #key for dict
+        
+        computed = self.computed.get(key, False)
+        
+        if computed:
+            return computed #return already computed result
+        else:
+            q = queue.PriorityQueue()
+            q.put( (0, 0, (state, {}) ) ) # ( (state, accumulated cost), path)
+            path = self.get_path(q)
+            self.computed.update(path)
+            return path.get(key) #value associated to key (position, food)
+    
+    def get_path(self, list_visited):
+        """
+        Given a pacman game state, returns a legal move.
+        Arguments:
+        ----------
+        - `state`: the current game state. See FAQ and class
+                   `pacman.GameState`.
+        Return:
+        -------
+        - A legal move as defined in `game.Directions`.
+        """
+        while True:
+            i = 0 # Implicit value to avoid identical priority in the priority queue
+            entry = list_visited.get() # Pop out the priority element of the priority queue
+            print(entry)
+            state = entry[2][0] # Consider the state linked to the first element
+            cost = entry[0] # Accumulated cost for this state
+            visited = entry[2][1] # Path to the state as dict {key : actions}
+            key = (state.getPacmanPosition(), state.getFood()) # for dict
+            
+            if state.isWin():
+                visited[key] = dir.STOP # add {(s.Pacman pos , s.food) : action} to the returned dict
+                return visited
+            
+            else:
+                successors = state.generatePacmanSuccessors()
+                
+                for son in successors:
+                    i+=1
+                    visited_aux = visited.copy() # Buffer to avoid losing original dict data after the 1st son
+                    visited_aux[key] = son[1] # Add the action of the current state
+                    new_cost = cost + self.branch_cost(son[0].getPacmanPosition(), son[0].getFood())
+                    list_visited.put( (new_cost, i, ( son[0], visited_aux)) )
+    
+    
+    def branch_cost(self, pos, foods):
+        """
+        Given a pacman position and a food matrix, returns the shortest
+        distance to a dot.
+        Arguments:
+        ----------
+        - `pos`: Pacman position as a pair (x,y) x,y >= 0
+        - `food`: matrix of booleans indicating by True values the presence 
+        of a dot in the maze.
+        Return:
+        -------
+        - A integer representing the shortest distance to a dot
+        """
+        foods_pos = []
+        i=0 # x values
+        for rows in foods:
+            j=0 # y values
+            for elem in rows:
+                if elem:
+                    foods_pos.append((i,j))
+                j+=1
+            i+=1
+
+        distances = list( map( lambda x : abs(pos[0] - x[0]) + abs(pos[1] - x[1]) ,foods_pos) )
+        return min(distances)
