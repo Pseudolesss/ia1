3bce211a5099d218b29ad8bcbdeb23dcd2ed1ac8
Pseudoless <julienlhoest@hotmail.com>
2018-10-11 17:53:30 +0200

    alright version (with a unused def)

--- ucs.py
+++ ucs.py
@@ -11,7 +11,8 @@ class PacmanAgent(Agent):
         ----------
         - `args`: Namespace of arguments from command-line prompt.
         """
-        self.computed = dict()
+        self.computed = dict() # Dict to store computed actions {(s.PacmanPos, s.food) : action}
+        self.visited = set() # Set to store the visited nodes
         self.args = args
 
     def get_action(self, state):
@@ -32,13 +33,16 @@ class PacmanAgent(Agent):
         if computed:
             return computed #return already computed result
         else:
+            self.visited.clear() # Clear set if previously used
+            
             q = []
-            heappush(q, (0, (state, {})) )
+            heappush(q,(0, (state, {})) ) # Initial state of the priority queue
             path = self.get_path(q)
-            self.computed.update(path)
+            
+            self.computed.update(path) # Keep computed answers
             return path.get(key) #value associated to key (position, food)
     
-    def get_path(self, list_visited):
+    def get_path(self, queue):
         """
         Given a pacman game state, returns a legal move.
         Arguments:
@@ -52,24 +56,38 @@ class PacmanAgent(Agent):
         i = 0 # Implicit value to avoid identical priority in the priority queue
         
         while True:
-            entry = heappop(list_visited) # Pop out the priority element of the priority queue
+            entry = heappop(queue) # Pop out the priority element of the priority queue
             state = entry[1][0] # Consider the state linked to the first element
             cost = entry[0] # Accumulated cost for this state
             visited = entry[1][1] # Path to the state as dict {key : actions}
             key = (state.getPacmanPosition(), state.getFood()) # for dict
 
-            successors = state.generatePacmanSuccessors()
+            if state.isWin():
+                visited[key] = dir.STOP
+                return visited
             
-            for son in successors:
-                if son[0].isWin():
-                    visited[key] = son[1] # add {(s.Pacman pos , s.food) : action} of the current node to the returned dict
-                    visited[(son[0].getPacmanPosition(), son[0].getFood())] = dir.STOP # of the final node
-                    return visited
-                i+=10**-12
-                visited_aux = visited.copy() # Buffer to avoid losing original dict data after the 1st son
-                visited_aux[key] = son[1] # Add the action of the current state
-                new_cost = cost//1 + self.branch_cost(son[0].getPacmanPosition(), son[0].getFood()) + i
-                heappush( list_visited, (new_cost, ( son[0], visited_aux)) )
+            else:
+                
+                if key in self.visited: # If actual state already visited
+                    continue # Ignore this node
+    
+                self.visited.add(key)
+    
+                successors = state.generatePacmanSuccessors()
+                
+                for son in successors:
+                    son_key = (son[0].getPacmanPosition(), son[0].getFood())
+                    if son_key in self.visited: #If son visited
+                        continue # Next son
+                    if son[0].isWin():
+                        visited[key] = son[1] # add {(s.Pacman pos , s.food) : action} of the current node to the returned dict
+                        visited[(son[0].getPacmanPosition(), son[0].getFood())] = dir.STOP # of the final node
+                        return visited
+                    i+=10**-12
+                    visited_aux = visited.copy() # Buffer to avoid losing original dict data after the 1st son
+                    visited_aux[key] = son[1] # Add the action of the current state
+                    new_cost = cost//1 + 1 + i # self.branch_cost(son_key[0], son_key[1])
+                    heappush( queue, (new_cost, ( son[0], visited_aux)) )
     
     
     def branch_cost(self, pos, foods):
